3e7117f57ee4c7d5ade7c04dafe1df7c36c01d90
Maelton-SI <mls54@aluno.ifal.edu.br>
2023-12-05 10:29:11 -0300

    first commit

---
+++ src/main/java/br/com/maelton/projeto_gestao_vagas/ProjetoGestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.maelton.projeto_gestao_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maelton/projeto_gestao_vagas/ProjetoGestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maelton.projeto_gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
